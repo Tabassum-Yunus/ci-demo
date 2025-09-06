ddf1321bf24f7361723989181260da19e649bb9d
Tabassum-Yunus <tabassumyunus09@gmail.com>
2025-09-06 12:12:10 +0530

    Change text

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from CI/CD demo!");
+  res.send("Hello CI/CD demo!");
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`App running on port ${PORT}`);
 });
